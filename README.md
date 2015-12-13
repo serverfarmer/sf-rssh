845067a418066f5f9984f2ea4be41b453d67b67d
Tomasz Klim <github@tomaszklim.pl>
2015-12-13 23:44:40 +0100

    first version

---
+++ README.md
@@ -0,0 +1,9 @@
+sf-rssh extension is responsible for installing and configuring restricted shell
+for OpenSSH. It is compatible with the following operating systems:
+
+- Debian 6.x (Squeeze)
+- Debian 7.x (Wheezy)
+- Debian 8.x (Jessie)
+- Ubuntu 14.04 LTS (Trusty Tahr)
+
+rsync, scp and sftp are enabled for all users. Everything else is disabled.
---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+. /opt/farm/scripts/init
+. /opt/farm/scripts/functions.install
+
+
+
+base=/opt/sf-rssh/templates/$OSVER
+
+if [ ! -f $base/rssh.tpl ]; then
+	echo "skipping rssh setup, unsupported operating system version"
+	exit 1
+fi
+
+install_deb rsync
+install_deb rssh
+
+save_original_config /etc/rssh.conf
+install_copy $base/rssh.tpl /etc/rssh.conf
+
+mkdir -p /srv/rsync

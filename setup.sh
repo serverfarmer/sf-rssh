245594e588e29ef30e57a54065fae339c981f9a4
Tomasz Klim <github@tomaszklim.pl>
2018-02-08 01:55:03 +0100

    pass multiple package names to install script

--- setup.sh
+++ setup.sh
@@ -11,8 +11,7 @@ if [ ! -f $base/rssh.tpl ]; then
 	exit 1
 fi
 
-/opt/farm/ext/repos/utils/install.sh rsync
-/opt/farm/ext/repos/utils/install.sh rssh
+/opt/farm/ext/repos/utils/install.sh rsync rssh
 
 save_original_config /etc/rssh.conf
 install_copy $base/rssh.tpl /etc/rssh.conf
